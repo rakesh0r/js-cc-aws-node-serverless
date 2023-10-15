c42733e36b4b13d5911428b45f648607dd8b2167
Rakesh Rekala <rakesh_rekala@epam.com>
2023-10-15 14:20:47 +0530

    Task-4 | Integration with NoSQL Database

---
+++ product-service/scripts/stocks.sql
@@ -0,0 +1,8 @@
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73c48a80aa', 'count': 4}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73c48a80a0', 'count': 10}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73c48a80a2', 'count': 23}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73c48a80a1', 'count': 15}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73c48a80a3', 'count': 23}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9345-fc73348a80a4', 'count': 15}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-48c5-9445-fc73c48a80a5', 'count': 23}
+INSERT INTO "stocks" value {'product_id': '7567ec4b-b10c-45c5-9345-fc73c48a80a6', 'count': 15}
